5edb527b14f8b37fb5323ac6ee78ecda98b39705
rajagopalparimi <rajparimi99@gmail.com>
2017-12-23 15:49:25 -0800

    perfect

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication4
+namespace CsharpPrograms
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            int x;
+            int y;
+            int result;
+            Console.Write("\n Enter the first number to be added: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the second number to be added: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            result = x + y;
+            Console.Write("\n The sum of two numbers is: " + result);
+            Console.ReadLine();
         }
     }
 }
