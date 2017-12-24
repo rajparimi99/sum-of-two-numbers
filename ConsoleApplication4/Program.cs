76072ededfc5cfd2bba3f70a88d40440d209ce51
rajagopalparimi <rajparimi99@gmail.com>
2017-12-24 10:21:29 -0800

    variable changed

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -8,13 +8,13 @@ namespace CsharpPrograms
         {
 
             int x;
-            int y;
+            int z;
             int result;
             Console.Write("\n Enter the first number to be added: ");
             x = Convert.ToInt32(Console.ReadLine());
             Console.Write("\n Enter the second number to be added: ");
-            y = Convert.ToInt32(Console.ReadLine());
-            result = x + y;
+            z = Convert.ToInt32(Console.ReadLine());
+            result = x + z;
             Console.Write("\n The sum of two numbers is: " + result);
             Console.ReadLine();
         }
